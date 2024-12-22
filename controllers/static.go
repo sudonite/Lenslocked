cc2f00070eedfff649ffe3fb483676a22b8c56e7
sudonite <sudonite@gmail.com>
2024-12-22 07:37:27 +0100

    Add: Signup Page

---
+++ controllers/static.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func StaticHandler(tpl Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+		{
+			Question: "Where is your office located?",
+			Answer:   "Our entire team is remote!",
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/sudonite/lenslocked/controllers"
+	"github.com/sudonite/lenslocked/templates"
+	"github.com/sudonite/lenslocked/views"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS, "home.gohtml", "tailwind.gohtml",
+	))))
+
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS, "contact.gohtml", "tailwind.gohtml",
+	))))
+
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS, "faq.gohtml", "tailwind.gohtml",
+	))))
+
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
+}
